dca9bbe83423f31755829a64f0a1d37e13ff08dc
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-08-08 20:29:18 +0000

    Created various fail java files
    feat: Created multiple fail state files
    chore: Added test script for running unit tests

---
+++ Project1/src/ProjectTest.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class ProjectTest {
+    @Test 
+    public void JVValid() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/src/Project.java");
+            assertEquals(true, jv.fileCharacters.length > 0);
+        } catch(Exception e) {
+            assertEquals(0, 1);
+        }
+    }
+
+    @Test 
+    public void JVInvalidFile() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/asd.java");
+            assertEquals(null, jv);
+        } catch(Exception e) {
+            assertEquals(1, 1);
+        }
+    }
+    
+    @Test 
+    public void JVFailClose() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Close_Bracket.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+
+    @Test 
+    public void JVFailMissingOpen() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Open_Bracket.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+
+    @Test 
+    public void JVFailMissingOpenBrace() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Open_Brace.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+    
+    @Test 
+    public void JVFailMissingCloseBrace() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Close_Brace.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+
+    @Test 
+    public void JVFailQuote() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Quote.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+    
+    @Test 
+    public void JVFailChar() {
+        try {
+            JavaValidator jv = new JavaValidator("/workspaces/CMSC315/Project1/TestFiles/Test_Fail_Char_Quote.java");
+            assertEquals(false, jv.validateFile());
+        } catch(Exception e) {
+            assertEquals(false, true);
+        }
+    }
+}
