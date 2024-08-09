9408993ebcc86534cc023e51e1269e94e873668f
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-08-09 16:18:44 +0000

    Added Constructor and Overrided toString
    feat: Created constructor for Delimiter
    feat: Overrided toString to return a formatted message

--- Project1/src/Delimiter.java
+++ Project1/src/Delimiter.java
@@ -1,4 +1,17 @@
 public class Delimiter {
     public Character character;
     public int index;
+    public int delimeterIndex;
+    public int lineNumber;
+
+    public Delimiter(Character c, int i, int di, int ln) {
+        character = c;
+        index = i;
+        delimeterIndex = di;
+        lineNumber = ln;
+    }
+
+    public String toString() {
+        return "Char: " + character + " File Index: " + index + " Delimiter Index: " + delimeterIndex + " Line: " + lineNumber;
+    }
 }
