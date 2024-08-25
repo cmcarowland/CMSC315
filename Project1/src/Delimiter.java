da7754ded79d52f0eed802a27ae022f6827cd8d4
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-08-25 18:45:24 +0000

    Updated Delimiter class
    feat: Added description array and getter.  This better facilitates building error messages

--- Project1/src/Delimiter.java
+++ Project1/src/Delimiter.java
@@ -1,8 +1,23 @@
+/*
+ * Raymond Rowland
+ * Project 1
+ * 8/25/2024
+ * 
+ * Represents a delimiter character in a text file.
+ * The class stores details about the delimiter including the character itself,
+ * its position in the file, its type, and the line number where it appears.
+ * 
+ * The delimiter type is described by an index into the delimiterDescription
+ * array, which includes types such as "Brace", "Parenthesis", "Bracket", etc.
+ */
+
 public class Delimiter {
     public Character character;
     public int index;
     public int delimeterIndex;
     public int lineNumber;
+
+    private String[] delimiterDescription = new String[] {"Brace", "Parenthese", "Bracket", "Quote", "Character", "Comment", "Block Comment"};
 
     public Delimiter(Character c, int i, int di, int ln) {
         character = c;
@@ -11,7 +26,12 @@ public class Delimiter {
         lineNumber = ln;
     }
 
+    @Override
     public String toString() {
-        return "Char: " + character + " File Index: " + index + " Delimiter Index: " + delimeterIndex + " Line: " + lineNumber;
+        return "Opened " + delimiterDescription[delimeterIndex] + " " + "Char: '" + character + "' File Index: " + index + " Delimiter Index: " + delimeterIndex + " Line: " + lineNumber;
+    }
+
+    public String getDescription() {
+        return delimiterDescription[delimeterIndex];
     }
 }
