546d02a650c1c5b77ac0b1e853bb3113fb9b15c8
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-09-05 00:10:59 +0000

    Initial commit for project 2

---
+++ Project2/src/PaneExample.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class PaneExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+
+        Pane  pane  = new Pane();
+
+        pane.getChildren().add(new Label("Hello Pane"));
+
+        Scene scene = new Scene(pane);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
