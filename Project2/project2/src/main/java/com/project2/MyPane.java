acca597024813ec33ef22570d6b9ca5b6b82974c
Quatro Designs <cmcarowland@live.com>
2024-09-07 15:35:03 -0400

    Updated MyPane
    feat: Changed the background to dark grey
    feat: Created method for computingMaximal
    refactor: Reorginized redraw so the lines are under the circles
    feat: Color the max circles yellow
    feat: Draw lines between the max points

--- Project2/project2/src/main/java/com/project2/MyPane.java
+++ Project2/project2/src/main/java/com/project2/MyPane.java
@@ -1,8 +1,13 @@
 package com.project2;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -13,6 +18,9 @@ public class MyPane extends Pane {
     private LinkedList<Point> points; 
 
     public MyPane (Point[] inPoints) {
+        BackgroundFill bgf = new BackgroundFill(Color.valueOf("#202020"), null, getInsets());
+        Background bg = new Background(bgf);
+        setBackground(bg);
         setOnMouseClicked(event -> reactToMouseClick(event));
         points = new LinkedList<Point>();
         for(var p : inPoints)
@@ -38,24 +46,72 @@ public class MyPane extends Pane {
         }
     }
 
+    //O(n * n)
+    private void computeMaximal() {
+        Point currentPoint = null;
+        Point otherPoint = null;
+        for(int i = 0; i < points.size(); i++) {
+            currentPoint = points.get(i);
+            if(!currentPoint.isMaximal)
+                continue;
+            for(int j = 0; j < points.size(); j++) {
+                if(i == j)
+                    continue;
+
+                otherPoint = points.get(j);
+                if(otherPoint.isMaximal && currentPoint.isBelowAndLeft(otherPoint)) {
+                    otherPoint.isMaximal = false;
+                }
+            }
+        }
+    }
+
     private void redraw() {
         getChildren().clear(); 
+        points.stream().forEach(x -> x.isMaximal = true);
+        computeMaximal();
         Point currentPoint = null;
         Point lastPoint = null;
 
+        
+        List<Point> max = points.stream().filter(x->x.isMaximal).collect(Collectors.toList());
+        Collections.sort(max, Collections.reverseOrder());
+        for(int i = 0; i < max.size(); i++) {
+            currentPoint = max.get(i);
+            if(i == 0) {
+                Line line = new Line(currentPoint.getX(), 500, currentPoint.getX(), currentPoint.getY());
+                getChildren().add(line);
+                if(i + 1 < max.size() - 1) {
+                    
+                }
+            } 
+            
+            if(i == max.size() - 1) {
+                Line line = new Line(0, currentPoint.getY(), currentPoint.getX(), currentPoint.getY());
+                getChildren().add(line);
+                continue;
+            }
+            
+            if(i < max.size() - 1) {
+                lastPoint = max.get(i + 1);
+                Line line = new Line(currentPoint.getX(), currentPoint.getY(), lastPoint.getX(), currentPoint.getY());
+                getChildren().add(line);
+                line = new Line(lastPoint.getX(), lastPoint.getY(), lastPoint.getX(), currentPoint.getY());
+                getChildren().add(line);
+            }
+        }
+        
         for(int i = 0; i < points.size(); i++) {
             lastPoint = currentPoint;
             currentPoint = points.get(i);
-            Circle dot = new Circle(currentPoint.getX(), currentPoint.getY(), 10, Color.BLACK);
+            Circle dot = new Circle(currentPoint.getX(), currentPoint.getY(), 5, Color.LIGHTBLUE);
+            if(currentPoint.isMaximal) 
+                dot.setFill(Color.YELLOW);
+    
             dot.setOnMouseReleased(event -> removeClick(event));
             
             currentPoint.setDot(dot);
             getChildren().add(dot);
-            
-            if(lastPoint != null) {
-                Line line = new Line(lastPoint.getX(), lastPoint.getY(), currentPoint.getX(), currentPoint.getY());
-                getChildren().add(line);
-            }
         }
     }
 }
