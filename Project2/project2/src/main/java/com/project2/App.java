7344ebb0cb8e83cb53d361f051f7168fd2989a95
Quatro Designs <cmcarowland@live.com>
2024-09-07 15:53:47 -0400

    Updated App to read input file
    feat: Created Input file points.txt
    feat: Added logic to draw initial points on the graph

--- Project2/project2/src/main/java/com/project2/App.java
+++ Project2/project2/src/main/java/com/project2/App.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * JavaFX App
@@ -11,16 +14,28 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Point[] points;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(new MyPane(new Point[]{new Point(200, 300), new Point(250, 300)}) , 500, 500);
+        scene = new Scene(new MyPane(points), 500, 500);
         stage.setTitle("Raymond Rowland Project 2");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        try {
+            String[] items = null;
+            List<String> fileLines = Files.readAllLines(Paths.get("/Volumes/Hard Drive/Documents/College/CMSC315/Project2/points.txt"));
+            points = new Point[fileLines.size()];
+            for(int i = 0; i < fileLines.size(); i++) {
+                items = fileLines.get(i).split(" ");
+                points[i] = new Point(Double.parseDouble(items[0]), Double.parseDouble(items[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         launch();
     }
 
