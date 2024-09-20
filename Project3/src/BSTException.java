6d151db65297170b287ffffe2716478610bc7de7
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-09-20 16:09:46 +0000

    Created a custom exception for BST Parsing
    feat: Class BSTException is a custom exception type for Binary search table parsing

---
+++ Project3/src/BSTException.java
@@ -0,0 +1,18 @@
+/*
+ * Raymond Rowland
+ * Project 3
+ * 9/18/2024
+ * 
+ * Incomplete Tree 
+ * Data is Not an Integer
+ * Extra Characters at the End 
+ * Missing Left Parenthesis 
+ * Missing Right Parentheses 
+ * 
+ */
+
+public class BSTException extends Exception {
+    public BSTException(String errorMessage) {
+        super(errorMessage);
+    }
+}
