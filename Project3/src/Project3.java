c2500efac4275ab28ca0a1e87e44ae36ccd5cc97
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-09-23 13:08:13 +0000

    Added comments and required output for project
    feat: Completd project output requirements
    chore: Added required commets section

--- Project3/src/Project3.java
+++ Project3/src/Project3.java
@@ -3,7 +3,10 @@
  * Project 3
  * 9/18/2024
  * 
- * 
+ * The Project3 class serves as the entry point for the binary search tree (BST) application.
+ * It prompts the user to input a binary tree string, validates it, and constructs the BST.
+ * The program checks if the tree is a valid BST and whether it is balanced, providing relevant
+ * output and allowing the user to input additional trees as desired.
  */
 
 
@@ -26,20 +29,27 @@ public class Project3 {
             }
 
             System.out.println(bst);
-            System.out.println("Height : " + bst.getHeight());
             if(bst.isBST()) {
                 if(bst.isBalanced()) {
                     System.out.println("It is a balanced binary search tree");
                 } else {
                     System.out.println("It is a binary search tree but it is not balanced");
+                    BST balancedBST = new BST(bst.getTreeAsArray());
+                    System.out.println(balancedBST);
+                    System.out.println("Original tree has height " + bst.getHeight());
+                    System.out.println("Balanced tree has height " + balancedBST.getHeight());
                 }
             } else {
                 System.out.println("It is not a binary search tree");
+                BST balancedBST = new BST(bst.getTreeAsArray());
+                System.out.println(balancedBST);
+                System.out.println("Original tree has height " + bst.getHeight());
+                System.out.println("Balanced tree has height " + balancedBST.getHeight());
             }
         } while(GetUserInput("More Trees? Y or N: ").toLowerCase().equals("y"));
     }
 
-    public static String GetUserInput(String question) {
+    private static String GetUserInput(String question) {
         System.out.print(question);
         return scanner.nextLine().strip();  
     }
