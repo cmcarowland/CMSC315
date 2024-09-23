5cb764124dca91b42698ee264775b94f8fb9daaa
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-09-23 13:09:25 +0000

    Broke tests into smalled sections
    refactor: Changed test all to several smaller tests for proper validation
    chore: Added required comment block

--- Project3/src/Project3Test.java
+++ Project3/src/Project3Test.java
@@ -1,3 +1,16 @@
+/*
+ * Raymond Rowland
+ * Project 3
+ * 9/23/2024
+ * 
+ * The Project3Test class contains unit tests for the Project3 application,
+ * validating various scenarios for binary search tree input. Tests cover 
+ * cases such as complete trees, incomplete trees, invalid data, and extra
+ * characters, ensuring that appropriate exceptions and messages are generated.
+ * The tests also verify the correct structure and balance of the constructed 
+ * trees, using regex patterns to match expected outputs.
+ */
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import java.util.regex.Matcher;
@@ -6,7 +19,7 @@ import java.util.regex.Pattern;
 public class Project3Test {
 
     @Test 
-    public void TestAll() {
+    public void TestComplete() {
         Pattern pattern = Pattern.compile("It is a balanced binary search tree", Pattern.CASE_INSENSITIVE);
         MockInOut mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * *) *))\nn\n");
         Project3.main(new String[0]);
@@ -15,68 +28,89 @@ public class Project3Test {
         System.out.println(x);
         Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
+    }
        
-        pattern = Pattern.compile("Tree is incomplete", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 *) (41 * *)) (83 (67 * *) *))\nn\n");
+    @Test 
+    public void TestIncomplete() {
+        Pattern pattern = Pattern.compile("Tree is incomplete", Pattern.CASE_INSENSITIVE);
+        MockInOut mio = new MockInOut("(53 (28 (11 *) (41 * *)) (83 (67 * *) *))\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x = mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-        
-        pattern = Pattern.compile("Data is not an Integer", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * A) *))\nn\n");
+    }
+
+    @Test 
+    public void TestInvalidData() {
+        Pattern pattern = Pattern.compile("Data is not an Integer", Pattern.CASE_INSENSITIVE);
+        MockInOut mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * A) *))\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x = mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-        
-        pattern = Pattern.compile("Extra Characters at the End", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * *) *))1234\nn\n");
+    }
+
+    @Test 
+    public void TestExtra() {
+        Pattern pattern =Pattern.compile("Extra Characters at the End", Pattern.CASE_INSENSITIVE);
+        MockInOut mio =new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * *) *))1234\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x =mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-       
-        pattern = Pattern.compile("Missing Left Parenthesis", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("53 (28 (11 * *) (41 * *)) (83 (67 * *) *))\nn\n");
+    }
+     
+    @Test 
+    public void TestMissingLeft() {
+        Pattern pattern =Pattern.compile("Missing Left Parenthesis", Pattern.CASE_INSENSITIVE);
+        MockInOut mio =new MockInOut("53 (28 (11 * *) (41 * *)) (83 (67 * *) *))\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x =mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-       
-        pattern = Pattern.compile("Missing Left Parenthesis", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 * *) (41 * *)) 83 (67 * *) *))\nn\n");
+    }
+
+    @Test 
+    public void TestMissingLeftInside() {
+        Pattern pattern =Pattern.compile("Missing Left Parenthesis", Pattern.CASE_INSENSITIVE);
+        MockInOut mio =new MockInOut("(53 (28 (11 * *) (41 * *)) 83 (67 * *) *))\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x =mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-      
-        pattern = Pattern.compile("Missing Right Parenthesis", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * * *)) \nn\n");
+    }  
+    
+    @Test 
+    public void TestMissingRight() {
+        Pattern pattern =Pattern.compile("Missing Right Parenthesis", Pattern.CASE_INSENSITIVE);
+        MockInOut mio =new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * * *)) \nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x =mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
-        
-        pattern = Pattern.compile("Missing Right Parenthesis", Pattern.CASE_INSENSITIVE);
-        mio = new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * *) *)\nn\n");
+    }
+     
+    @Test 
+    public void TestRightInside() {
+        Pattern pattern =Pattern.compile("Missing Right Parenthesis", Pattern.CASE_INSENSITIVE);
+        MockInOut mio =new MockInOut("(53 (28 (11 * *) (41 * *)) (83 (67 * *) *)\nn\n");
         Project3.main(new String[0]);
-        x = mio.getOutput();
+        String x =mio.getOutput();
         mio.close();
         System.out.println(x);
-        matcher = pattern.matcher(x);
+        Matcher matcher = pattern.matcher(x);
         assertEquals(true, matcher.find());
     }
 
@@ -117,18 +151,18 @@ public class Project3Test {
         assertEquals(true, matcher.find());
         pattern = Pattern.compile("It is a binary search tree but it is not balanced", Pattern.CASE_INSENSITIVE);
         matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        assertEquals(true, matcher.find());
 
         pattern = Pattern.compile("20\\n" + //
                         "\\t13\\n" + //
-                        "\\t21\\n" + //
+                        "\\t51\\n" + //
                         "\\t\\t63", Pattern.CASE_INSENSITIVE);
-        // matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        matcher = pattern.matcher(x);
+        assertEquals(true, matcher.find());
 
         pattern = Pattern.compile("Original tree has height 3\nBalanced tree has height 2", Pattern.CASE_INSENSITIVE);
-        // matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        matcher = pattern.matcher(x);
+        assertEquals(true, matcher.find());
     }
 
     @Test 
@@ -147,17 +181,17 @@ public class Project3Test {
         assertEquals(true, matcher.find());
         pattern = Pattern.compile("It is not a binary search tree", Pattern.CASE_INSENSITIVE);
         matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        assertEquals(true, matcher.find());
 
         pattern = Pattern.compile("13\\n" + //
                         "\\t11\\n" + //
                         "\\t53\\n" + //
                         "\\t\\t59", Pattern.CASE_INSENSITIVE);
         matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        assertEquals(true, matcher.find());
 
-        pattern = Pattern.compile("Original tree has height 3\nBalanced tree has height 2", Pattern.CASE_INSENSITIVE);
+        pattern = Pattern.compile("Original tree has height 2\nBalanced tree has height 2", Pattern.CASE_INSENSITIVE);
         matcher = pattern.matcher(x);
-        // assertEquals(true, matcher.find());
+        assertEquals(true, matcher.find());
     }
 }
